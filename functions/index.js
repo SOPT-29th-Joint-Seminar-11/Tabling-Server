44a09748335399fb515fb4412e40a9c26b1a6e57
seohyun-106 <changseohyun@sookmyung.ac.kr>
2021-11-19 18:45:11 +0900

    [init] git init

---
+++ functions/index.js
@@ -0,0 +1,18 @@
+const admin = require("firebase-admin");
+const serviceAccount = require("./joint-seminar-tabling-firebase-adminsdk-g4im5-69afb96a88");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+let firebase;
+if (admin.apps.length === 0) {
+  firebase = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+} else {
+  firebase = admin.app();
+}
+
+module.exports = {
+  api: require("./api"),
+};
